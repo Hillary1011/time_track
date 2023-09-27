b2b707c637ac5375c4230af37e728ae505b63cf6
Hillary1011 <hill64maps7299@gmail.com>
2023-09-27 16:46:08 +0800

    Version 4

--- scr/script101.py
+++ scr/script101.py
@@ -4,6 +4,9 @@ now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
 
+def get_language():
+    pass
+
 class TimeC():
     def __init__(self, label):
         self.current_time = current_time
