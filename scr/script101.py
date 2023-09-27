f8421cc0fcdad7380f5f196c5d0a638464efa95b
Hillary1011 <hill64maps7299@gmail.com>
2023-09-27 15:30:04 +0800

    Added Time class

--- scr/script101.py
+++ scr/script101.py
@@ -4,4 +4,7 @@ now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
 
-
+class TimeC():
+    def __init__(self, label):
+        self.current_time = current_time
+        self.label = label
